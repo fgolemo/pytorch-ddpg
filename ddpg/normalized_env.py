4820e8cc528469679ad915e7cee518123f2937e3
Florian Golemo <fgolemo@gmail.com>
2018-01-09 16:53:01 -0500

    added observation flattening

--- ddpg/normalized_env.py
+++ ddpg/normalized_env.py
@@ -14,3 +14,9 @@ class NormalizedEnv(gym.ActionWrapper):
         act_k_inv = 2./(self.action_space.high - self.action_space.low)
         act_b = (self.action_space.high + self.action_space.low)/ 2.
         return act_k_inv * (action - act_b)
+
+class NormalizedObsEnv(gym.ObservationWrapper):
+
+    def _observation(self, observation):
+        if len(observation.shape) > 1:
+            return observation.reshape(-1) # merge all dimensions
